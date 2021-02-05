92c87cbe6ae0ef8fd4e7d06a7c6f60fcb963e3f1
Cyrus Goh <hello@lovincyrus.com>
2021-02-05 10:37:58 -0800

    tweak messari screener disclaimer font

--- src/components/sections/SectionMessari.vue
+++ src/components/sections/SectionMessari.vue
@@ -4,7 +4,7 @@
     p Explore an ever-expanding ecosystem of blockchain apps, powered by Cosmos technology.
     tm-btn(size="lg" value="view stats" type="anchor" href="https://messari.io/screener/cosmos-ecosystem-FCB5C9E8" target="_blank" rel="noreferrer noopener"
       icon="north_east" icon-pos="right")
-    .note Screener powered by Messari
+    .disclaimer Screener powered by Messari
     div(slot="image")
       .phone: img(src="~assets/images/ecosystem/messari-screener.svg" alt="Messari screener")
       .tablet: img(src="~assets/images/ecosystem/messari-screener.svg" alt="Messari screener")
@@ -29,8 +29,11 @@ export default {
   background url('~images/site/bg-tile-stars.svg'), linear-gradient(135deg, rgb(15,13,32,1) 0%, rgb(4,4,5) 25%, rgb(33,34,77) 53%, rgb(68,110,183) 100%)
   background-size 256px 256px, auto
 
-.note
+.disclaimer
   margin-top var(--pad320)
+  font-size 0.8125rem
+  font-weight 400
+  line-height 1.125rem
 
 @media screen and (min-width: 1024px)
   #section-messari
